5433475f18ea71fabd3affff84f7626f212cf52a
Byron Quezada <byque2013@gmail.com>
2021-02-13 17:14:29 -0500

    Crear primer guión

---
+++ primer_guion.ps1
@@ -0,0 +1,8 @@
+﻿# Basado en:
+# https://www.windowscentral.com/how-create-and-run-your-first-powershell-script-file-windows-10
+#
+# Recordar que en Windows los guiones se deben guardar con codificación UTF-8
+# con BOM para que la terminal reproduzca correctamente los caracteres.
+
+$mensaje = "¡Felicitaciones! Tu primer guión se ejecutó exitosamente."
+Write-Host $mensaje
