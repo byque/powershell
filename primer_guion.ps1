b4f37cd017a9b51334ee5de936d2237a8a22557f
Byron Quezada <byque2013@gmail.com>
2021-02-13 19:44:17 -0500

    Añadir impresión de fecha

--- primer_guion.ps1
+++ primer_guion.ps1
@@ -8,3 +8,6 @@ Write-Host "¡Felicitaciones! Tu primer guión se ejecutó exitosamente."
 
 $mensaje = "¡Genial!"
 Write-Host "Tu mensaje es ->" $mensaje "<-"
+
+$fecha = Get-Date
+Write-Host "La fecha es" $fecha.ToString("yyyy/MM/dd")
