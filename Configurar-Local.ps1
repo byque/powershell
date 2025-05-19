f0ca3ec01e43fa247749df6027b62202f3e806a8
Byron Quezada <byronquezada@live.com>
2025-05-19 10:52:30 -0400

    Añadir instalación de Visual Studio Code

--- Configurar-Local.ps1
+++ Configurar-Local.ps1
@@ -1,3 +1,6 @@
 # Solicita permisos de administrador
 Write-Output "Instalar Joplin"
 winget install --id Joplin.Joplin --version 3.3.12
+
+Write-Output "Instalar Visual Studio Code"
+winget install --id Microsoft.VisualStudioCode --version 1.100.2
