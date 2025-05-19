7d3152ac05e1a4a4d3668e9873c3300c61809b1e
Byron Quezada <byronquezada@live.com>
2025-05-19 10:56:05 -0400

    Añadir instalación de Git

--- Configurar-Local.ps1
+++ Configurar-Local.ps1
@@ -1,4 +1,8 @@
 # Solicita permisos de administrador
+Write-Output "Instalar Git"
+winget install --id Git.Git --version 2.49.0
+
+# Solicita permisos de administrador
 Write-Output "Instalar Joplin"
 winget install --id Joplin.Joplin --version 3.3.12
 
