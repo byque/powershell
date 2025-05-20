a07e779773058a7a9c9ea534a4cd0c96fbe50302
Byron Quezada <byronquezada@live.com>
2025-05-19 22:58:20 -0400

    Añadir instalación de VirtualBox

--- Configurar-Local.ps1
+++ Configurar-Local.ps1
@@ -10,5 +10,9 @@ winget install --id Git.Git --version 2.49.0
 Write-Output "Instalar Joplin"
 winget install --id Joplin.Joplin --version 3.3.12
 
+# Solicita permisos de administrador
+Write-Output "Instalar VirtualBox"
+winget install --id Oracle.VirtualBox --version 7.1.8
+
 Write-Output "Instalar Visual Studio Code"
 winget install --id Microsoft.VisualStudioCode --version 1.100.2
